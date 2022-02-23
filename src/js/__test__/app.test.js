2995f7740be69187d8b5e2aec02162be3e07e683
Polina Vylizhanina <vylizhaninaps@gmail.com>
2022-02-23 11:15:08 +0500

    change of test

--- src/js/__test__/app.test.js
+++ src/js/__test__/app.test.js
@@ -2,16 +2,21 @@ import GameSavingLoader from '../gameSavingLoader';
 
 test(
   'Should work correct',
-  () => GameSavingLoader.load().then((data) => {
-    expect(data).toEqual({
-      id: 9,
-      created: 1234560800,
-      userInfo: {
-        id: 1,
-        name: 'Zombie',
-        level: 10,
-        points: 2000,
-      },
-    });
-  }),
+  async () => {
+    try {
+      const data = await GameSavingLoader.load();
+      expect(data).toEqual({
+        id: 9,
+        created: 1234560800,
+        userInfo: {
+          id: 1,
+          name: 'Zombie',
+          level: 10,
+          points: 2000,
+        },
+      });
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
 );
